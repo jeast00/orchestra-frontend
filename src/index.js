7e535e45b20ad5380020df3c0c71e573c80f9765
Jonathan East <jonathan_east@ymail.com>
2021-01-13 12:25:35 -0500

    added a const variable to grab the div id set in index.html for band names

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+
+// load the DOM content with an anonymous function
 document.addEventListener("DOMContentLoaded", () => {
     // add functions to load to page
     debugger;
 })
+
+//create const variables 
+const bandNameDivContainer = document.getElementById("band-name-div-container")
 
