4a65f780bf66a689ea6c1aa85c2fd55a5d3e9cd2
Jonathan East <jonathan_east@ymail.com>
2021-01-13 17:45:47 -0500

    added createBandForm function in index.js file; checked index.html for form on page

--- src/band.js
+++ src/band.js
@@ -4,10 +4,11 @@ class Band {
         this.band_name = band_name
     }
 
+   
     // render the band names to the page
 
     renderBandNames() {
-        const bandNameDivContainer = document.getElementById("band-name-div-container")
+        let bandNameDivContainer = document.getElementById("band-name-div-container")
         bandNameDivContainer.innerHTML += `<h3> ${this.band_name} </h3>`
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // add functions to load to page
     // debugger;
+    createBandNameForm();
     fetchBandNames();
 })
 
@@ -11,14 +12,29 @@ const main_url = "http://localhost:3000"
 
 //create functions 
 
+
+// fetch band name form create function
+function createBandNameForm() {
+    let bandNameFormDivContainer = document.getElementById("band-name-form-div-container")
+    bandNameFormDivContainer.innerHTML += `<form>
+                                            <h2>Add a Band Here:</h2>
+                                            <input type="text" id="band-name-input">
+                                            <input type="submit" value="Add Band">
+                                          </form>`
+    
+}
+
+
+
+// fetch band names function
 function fetchBandNames() {
     fetch(`${main_url}/bands`)
     .then(resp => resp.json())
     .then(bands => {
         for(const band of bands) {
-            let new_band = new Band(band.id, band.band_name)
-            new_band.renderBandNames();
-
+            let bandName = new Band(band.id, band.band_name)
+            // console.log(bandName);
+            bandName.renderBandNames();
         }
     })
 }
