035ba511f226e06b1d56387dfa3e50a1e965b274
Jonathan East <jonathan_east@ymail.com>
2021-01-14 08:48:09 -0500

    added renderBandNames function to Band class

--- src/band.js
+++ src/band.js
@@ -5,4 +5,9 @@ class Band {
 
 
     // render bands
+
+    renderBandNames() {
+        let bandNameDivContainer = document.getElementById("band-name-div-container")
+        bandNameDivContainer.innerHTML += `<h3>${this.band_name}</h3>`
+    }
 }
--- src/index.js
+++ src/index.js
@@ -8,5 +8,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
 const main_url = "http://localhost:3000"
 
+//Create functions
 
+// ------------------------------- //
+
+// fetch band names function
+
+// create a band name form function
+
+// create a submit function for the band form
+
+// delete a band name function
+
+// -------------------------------- //
+
+// fetch instrument names function and show them when a band name is clicked
+
+// create sounds for each instrument when clicked function
 
