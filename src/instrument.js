202885316d245f6cd8975392f6700d3034cb1349
Jonathan East <jonathan_east@ymail.com>
2021-01-14 08:39:21 -0500

    added instrument class to instrument.js file and script src file to index.html

---
+++ src/instrument.js
@@ -0,0 +1,8 @@
+class Instrument {
+    constructor(instrument_name, band_id) {
+        this.instrument_name = instrument_name
+        this.band_id = band_id
+    }
+
+    // render the instruments under each band
+}
