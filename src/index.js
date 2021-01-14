28e6bb3d43906b87b3b8c611b0447e80fefebedd
Jonathan East <jonathan_east@ymail.com>
2021-01-14 09:32:48 -0500

    working on submitBandForm function; need to check event listener for submit button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // debugger;
     fetchBandNames();
     createBandNameForm();
+    submitBandForm();
 })
 
 //create const variables 
@@ -30,15 +31,29 @@ function fetchBandNames() {
 
 // create a band name form function
 function createBandNameForm() {
+    // get the id of the form container from the html
     let bandNameFormDivContainer = document.getElementById("band-name-form-div-container")
     bandNameFormDivContainer.innerHTML += `<form>
                                            <h2>Add a Band Here:</h2>
                                            <input type="text" id="band-name-input">
                                            <input type="submit" value="Add Band">
                                            </form>`
+
+    // add event listener for the submit function from the form container variable
+    
 }
 
 // create a submit function for the band form
+
+function submitBandForm() {
+    // Submit button is used - need to use prevent default to stop refresh
+    event.preventDefault();
+    // needs to be a post request
+    // need to collect the value of the form input text box and display the value on the screen
+    let bandNameValue = document.getElementById("band-name-input").value
+    console.log(bandNameValue);
+}
+
 
 // delete a band name function
 
