e152eb901716ae8515d6386cf18f45d182c2be84
Jonathan East <jonathan_east@ymail.com>
2021-01-13 22:11:08 -0500

    changed index.html; added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 
 
 //create const variables 
-const main_url = "http://localhost:3000"
+const bandNameForm = document.getElementById("band-name-form")
+const bandNameValue = document.getElementById("band-name-input")
+// const main_url = "http://localhost:3000"
 
 //create functions 
 
+bandNameForm.addEventListener("submit", submitBandName)
 
-// fetch band name form create function
-function createBandNameForm() {
-    let bandNameFormDivContainer = document.getElementById("band-name-form-div-container")
-    bandNameFormDivContainer.innerHTML += `<form>
-                                            <h2>Add a Band Here:</h2>
-                                            <input type="text" id="band-name-input">
-                                            <input type="submit" value="Add Band">
-                                          </form>`
-
-    bandNameFormDivContainer.addEventListener("submit", submitBandName)
-    
-}
 
 function submitBandName() {
     event.preventDefault();
-    let bandNameValue = document.getElementById("band-name-input").value
-    console.log(bandNameValue);
+    // console.log(bandNameValue.value);
+    const header_3 = document.createElement('h3')
+    const p = document.createElement('p')
+    p.innerText = bandNameValue.value
+    // console.log(p.innerText);
+    header_3.appendChild(p)
+    bandNameForm.appendChild(header_3)
+
+    bandNameForm.reset()
+
 }
 
-createBandNameForm();
 
 
 
