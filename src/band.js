6956352d685f3babd7b9867e8b087287acc4ea7b
Jonathan East <jonathan_east@ymail.com>
2021-01-16 12:23:23 -0500

    creating DOM elements for band names and a delete button; testing code in console.log

--- src/band.js
+++ src/band.js
@@ -13,7 +13,21 @@ class Band {
         //create a div inside the band div container - create element button - append to div
         // store dataset 
         //review study groups - general - check video study groups
-        bandNameDivContainer.innerHTML += `<h3>${this.band_name}</h3>
-                                          <button class="delete-band-button">Delete Band</button>`
+
+        // DO NOT USE!!
+        // bandNameDivContainer.innerHTML += `<h3>${this.band_name}</h3>
+        //                                   <button class="delete-band-button">Delete Band</button>`
+
+        let bandNameDiv = document.createElement('div')
+        let bandNameHeader = document.createElement('h3')
+        let bandNameDeleteButton = document.createElement('button')
+        bandNameDiv.classList.add('band-name-class')
+        bandNameHeader.classList.add('band-name-header-3')
+        bandNameDeleteButton.classList.add('band-name-delete-button')
+        bandNameDiv.setAttribute('data-id', this.id)
+        bandNameDeleteButton.setAttribute('data-id', this.id)
+        console.log(bandNameDiv, bandNameHeader, bandNameDeleteButton);
+
+        debugger;
     }
 }
