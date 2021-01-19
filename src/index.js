c8fab086581640d571f2e90908cba401fda33377
Jonathan East <jonathan_east@ymail.com>
2021-01-19 10:29:56 -0500

    added function createBandForm and console logging elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 
+function createBandNameForm() {
+    const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
+    // console.log(bandNameFormDivContainer);
+    const bandNameFormTag = document.createElement('form')
+    console.log(bandNameFormTag);
+}
+
+createBandNameForm();
 
 
 
