145d8d1ef026a6f1900ae397954fce768e227c4f
Jonathan East <jonathan_east@ymail.com>
2021-01-20 14:15:37 -0500

    added innerHTML to append form to page; validated html to console.log

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 function createBandNameForm() {
     const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
+    bandNameFormDivContainer.innerHTML += `<form>
+                                            <h2>Add a Band Here:</h2>
+                                            <input type="text" id="band-name-input">
+                                            <input type="submit" value="Add Band">
+                                            </form>`
     console.log(bandNameFormDivContainer);
 }
 
