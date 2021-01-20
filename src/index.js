15cd6696054266e8db456524a99a02c156062d1b
Jonathan East <jonathan_east@ymail.com>
2021-01-20 14:06:05 -0500

    restarted and added event listener to load DOM content tested by debugger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-
-
-// create variables to obtain elements from html
-const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
-const bandNameInput = document.getElementById('band-name-input')
-const bandNameDiv = document.getElementById('band-name-div')
-
-
+document.addEventListener("DOMContentLoaded", ()=> {
+    debugger;
+})
 
 
 
