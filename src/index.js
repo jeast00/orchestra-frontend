b8f84681b7d0dbe3270f18c30f982fb78c67408c
Jonathan East <jonathan_east@ymail.com>
2021-01-20 12:38:50 -0500

    added html form to index.html; will work on pulling elements and adding dynamically js to index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // debugger;
-    createBandNameForm();
-})
 
-function createBandNameForm() {
-    const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
-    bandNameFormDivContainer.innerHTML += `<form>
-                                            <h2>Add a Band Here:</h2>
-                                            <input type="text" id="band-name-input">
-                                            <input type="submit" value="Add Band">
-                                            </form>`
-    bandNameFormDivContainer.addEventListener('submit', addBandName)
-}
 
-function addBandName() {
-    
-}
+
 
 
 
