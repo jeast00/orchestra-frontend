27e367d20b4157b7722a36f864f4f4a7d9fe3a74
Jonathan East <jonathan_east@ymail.com>
2021-01-20 12:42:49 -0500

    added variables to index.js to grab elements from index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 
 
+// create variables to obtain elements from html
+const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
+const bandNameInput = document.getElementById('band-name-input')
+const bandNameDiv = document.getElementById('band-name-div')
+
 
 
 
