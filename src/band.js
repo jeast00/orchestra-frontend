e4bd95715f892723f994e2fd1b82ade6fb78c048
Jonathan East <jonathan_east@ymail.com>
2021-01-20 15:36:56 -0500

    had to comment out other functions due to issue where renderBands was rendering 10 times for each band name 10 times; fixed issue in band.js where constructor was making the issue

--- src/band.js
+++ src/band.js
@@ -1,12 +1,23 @@
 class Band {
-    constructor(band) {
-        this.id = band.id
-        this.band_name = band.band_name
+    constructor(id, band_name) {
+        this.id = id
+        this.band_name = band_name
     }
 
     // render band names with instrument names for each band
     renderBands() {
         const bandNameDivContainer = document.getElementById('band-name-div-container')
+
+        const bandNameDiv = document.createElement('div')
+        bandNameDiv.setAttribute('id', 'band-name-div')
+        bandNameDiv.setAttribute('data-id', this.id)
+
+        const bandNameHeader3 = document.createElement('h3')
+        bandNameHeader3.setAttribute('id', 'band-name-header-3')
+        bandNameHeader3.innerText = `${this.band_name}`
+
+        bandNameDiv.appendChild(bandNameHeader3)
+        bandNameDivContainer.appendChild(bandNameDiv)
         console.log(bandNameDivContainer);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    createBandNameForm();
+    // createBandNameForm();
+    fetchBandNames();
     // addBandName();
 })
 
@@ -8,35 +9,54 @@ const main_url = "http://localhost:3000"
 
 // declare functions
 
-function createBandNameForm() {
-    const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
-    bandNameFormDivContainer.innerHTML += `<form>
-                                            <h2>Add a Band Here:</h2>
-                                            <input type="text" id="band-name-input">
-                                            <input type="submit" value="Add Band">
-                                            </form>`
-    console.log(bandNameFormDivContainer);
-    bandNameFormDivContainer.addEventListener('submit', addBandName)
-}
+// function createBandNameForm() {
+//     const bandNameFormDivContainer = document.getElementById('band-name-form-div-container')
+//     bandNameFormDivContainer.innerHTML += `<form>
+//                                             <h2>Add a Band Here:</h2>
+//                                             <input type="text" id="band-name-input">
+//                                             <input type="submit" value="Add Band">
+//                                             </form>`
+//     console.log(bandNameFormDivContainer);
+//     bandNameFormDivContainer.addEventListener('submit', addBandName)
+// }
 
-function addBandName() {
-    event.preventDefault();
-    const bandNameInput = document.getElementById('band-name-input').value 
-
-    let band = {
-        band_name: bandNameInput
-    }
-    fetch(`${main_url}/bands`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(band)
+function fetchBandNames() {
+    fetch(`${main_url}/bands`)
+    .then(resp => resp.json())
+    .then(bands => {
+        for(const band of bands) {
+            // console.log(band)
+            let bandName = new Band(band.id, band.band_name)
+            bandName.renderBands();
+        }
     })
-    .then(resp => console.log(resp))
-    // console.log(bandNameInput);
 }
+
+// function addBandName() {
+//     event.preventDefault();
+//     const bandNameInput = document.getElementById('band-name-input').value
+
+//     let band = {
+//         band_name: bandNameInput
+//     }
+
+//     console.log(band);
+
+//     fetch(`${main_url}/bands`, {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(band)
+//     })
+//     .then(resp => resp.json())
+//     .then(band => {
+//         let newBandName = new Band(band.id, band.band_name)
+//         newBandName.renderBands();
+//     })
+    // console.log(bandNameInput);
+// }
 
 
 
