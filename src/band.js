5a508fbe1c5b4b83f1ffd445ecaabfed3e4a8acd
Jonathan East <jonathan_east@ymail.com>
2021-01-22 06:25:25 -0500

    added a band class and band.js file; working on moving the render bands function to band.js; I believe this will resolve the issue with obtaining the band id integer needed to associate the band name to an id

---
+++ src/band.js
@@ -0,0 +1,11 @@
+class Band {
+    constructor(id, band_name) {
+        this.id = id
+        this.band_name = band_name
+    }
+
+
+    // add the render bands function from index.js to here
+
+    
+}
